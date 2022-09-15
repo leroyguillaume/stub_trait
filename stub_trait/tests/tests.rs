1b329a43290becc0b00251f47e0c7dbf6652b9c9
Guillaume Leroy <pro.guillaume.leroy@gmail.com>
2022-09-15 20:35:10 +0200

    test: add some tests

---
+++ stub_trait/tests/tests.rs
@@ -0,0 +1,52 @@
+use stub_trait::stub;
+
+#[stub]
+trait Animal {
+    fn name(&self) -> &str;
+}
+
+#[test]
+#[should_panic]
+fn should_panic_if_stub_all_calls_of_is_called_twice() {
+    let mut animal = StubAnimal::default();
+    animal.stub_all_calls_of_name(|| "Ivana");
+    animal.stub_all_calls_of_name(|| "Ivana");
+}
+
+#[test]
+#[should_panic]
+fn should_panic_if_stub_all_calls_of_is_called_after_register_stub_of() {
+    let mut animal = StubAnimal::default();
+    animal.register_stub_of_name(|| "Ivana");
+    animal.stub_all_calls_of_name(|| "Ivana");
+}
+
+#[test]
+#[should_panic]
+fn should_panic_if_register_stub_of_is_called_after_stub_all_calls_of() {
+    let mut animal = StubAnimal::default();
+    animal.stub_all_calls_of_name(|| "Ivana");
+    animal.register_stub_of_name(|| "Ivana");
+}
+
+#[test]
+fn should_stub_all_calls() {
+    let name = "Ivana";
+    let mut animal = StubAnimal::default();
+    animal.stub_all_calls_of_name(|| name);
+    assert_eq!(animal.name(), name);
+    assert_eq!(animal.name(), name);
+    assert_eq!(animal.count_calls_of_name(), 2);
+}
+
+#[test]
+fn should_stub_call_by_call() {
+    let name1 = "Ivana";
+    let name2 = "Truffle";
+    let mut animal = StubAnimal::default();
+    animal.register_stub_of_name(|| name1);
+    animal.register_stub_of_name(|| name2);
+    assert_eq!(animal.name(), name1);
+    assert_eq!(animal.name(), name2);
+    assert_eq!(animal.count_calls_of_name(), 2);
+}
